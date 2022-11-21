a8d718b9401e7424af672e93c7b42fc444263f32
Veronika G <nejnayanika@mail.ru>
2022-11-21 18:04:28 +0300

    bugfix

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # Проект: Mesto
 
-## Работа сделана с использованием технологий HTML и CSS(flexbox,Grid Layout.,transition, java script).
+## Работа сделана с использованием технологий HTML и CSS(flexbox,Grid Layout,transition, java script).
 
-
-* [Ссылка на проект](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
-
-
+[Ссылка на проект](https://veronikagg.github.io/mesto/index.html)
--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,27 @@
-const popupElement = document.querySelector(".popup");
-const popupCloseButtonElement = popupElement.querySelector(".popup__close");
-const popupOpenButtonElement = document.querySelector(".profile__edit-button");
-// let itemLikeElement = document.querySelector('.item__like');
-// let itemLikeActiveElement = document.querySelector('.item__like');
-// const inputContainerElement = popupElement.querySelector('.popup__input-container');
-const saveButton = popupElement.querySelector(".popup__button");
-const profileName = document.querySelector(".profile__name");
-const profileActivities = document.querySelector(".profile__activities");
-
-// function formSubmitHandler (evt) {
-//   evt.preventDefault();
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close');
+const popupEdit = document.querySelector('form');
+const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+const saveButton = popupElement.querySelector('.popup__button');
+const profileName = document.querySelector('.profile__name');
+const profileActivities = document.querySelector('.profile__activities');
+const popupName = popupElement.querySelector('.popup__input_type_name');
+const popupActivities = popupElement.querySelector('.popup__input_type_aboutme');
 
 const togglePopupVisibility = () => {
-  popupElement.classList.toggle("popup_opened");
+  popupElement.classList.toggle('popup_opened');
+  popupName.value = profileName.textContent;
+  popupActivities.value = profileActivities.textContent;
 };
-togglePopupVisibility();
 
-popupOpenButtonElement.addEventListener("click", togglePopupVisibility);
-popupCloseButtonElement.addEventListener("click", togglePopupVisibility);
+popupOpenButtonElement.addEventListener('click', togglePopupVisibility);
+popupCloseButtonElement.addEventListener('click', togglePopupVisibility);
 
-
-
-// блок редактирование профайла
-function addProfile() {
-  let popupName = document.querySelector(".popup__name");
-  let popupActivities = document.querySelector(".popup__aboutme");
-  profileName.textContent = `${popupName.value}`;
-  profileActivities.textContent = `${popupActivities.value}`;
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = popupName.value;
+  profileActivities.textContent = popupActivities.value;
   togglePopupVisibility();
 }
-saveButton.addEventListener("click", addProfile);
 
-// formElement.addEventListener('submit', formSubmitHandler);
+popupEdit.addEventListener('submit', formSubmitHandler);
