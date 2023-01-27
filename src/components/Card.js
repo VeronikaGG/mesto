f8a554b1c8256defb84de7362c5f2be4d9cc7b00
Veronika G <nejnayanika@mail.ru>
2023-01-27 13:11:17 +0300

    project_mesto_webpack

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,48 +1,48 @@
 export class Card {
-  constructor(data, handleOpenPopupImage, templateSelector) {
-    this._name = data.name;
-    this._link = data.link;
-    this._alt = data.name;
-    this._templateSelector = templateSelector;
-    this._handleOpenPopupImage = handleOpenPopupImage;
-  }
+    constructor(data, handleOpenPopupImage, templateSelector) {
+        this._name = data.name;
+        this._link = data.link;
+        this._alt = data.name;
+        this._templateSelector = templateSelector;
+        this._handleOpenPopupImage = handleOpenPopupImage;
+    }
 
-  _getTemplate() {
-    const cardElements = document.querySelector(this._templateSelector).content.querySelector('.item').cloneNode(true);
+    _getTemplate() {
+        const cardElements = document.querySelector(this._templateSelector).content.querySelector('.item').cloneNode(true);
 
-    return cardElements;
-  }
+        return cardElements;
+    }
 
-  generateCard() {
-    this._element = this._getTemplate();
-    this._setEventListeners();
-    this._element.querySelector('.item__name').textContent = this._name;
-    this._element.querySelector('.item__image').src = this._link;
-    this._element.alt = this._name;
-    this._like = this._element.querySelector('.item__like');
+    generateCard() {
+        this._element = this._getTemplate();
+        this._setEventListeners();
+        this._element.querySelector('.item__name').textContent = this._name;
+        this._element.querySelector('.item__image').src = this._link;
+        this._element.alt = this._name;
+        this._like = this._element.querySelector('.item__like');
 
-    return this._element;
-  }
+        return this._element;
+    }
 
-  _setEventListeners() {
-    this._element.querySelector('.item__like').addEventListener('click', () => {
-      this._handleLikeClick();
-    });
+    _setEventListeners() {
+        this._element.querySelector('.item__like').addEventListener('click', () => {
+            this._handleLikeClick();
+        });
 
-    this._element.querySelector('.item__trash').addEventListener('click', () => {
-      this._handleDeleteCard();
-    });
-    this._element.querySelector('.item__image').addEventListener('click', () => {
-      this._handleOpenPopupImage(this._link, this._name);
-    });
-  }
+        this._element.querySelector('.item__trash').addEventListener('click', () => {
+            this._handleDeleteCard();
+        });
+        this._element.querySelector('.item__image').addEventListener('click', () => {
+            this._handleOpenPopupImage(this._link, this._name);
+        });
+    }
 
-  _handleLikeClick() {
-    this._like.classList.toggle('item__like_active_black');
-  }
+    _handleLikeClick() {
+        this._like.classList.toggle('item__like_active_black');
+    }
 
-  _handleDeleteCard() {
-    this._element.remove();
-    this._element = null;
-  }
+    _handleDeleteCard() {
+        this._element.remove();
+        this._element = null;
+    }
 }
